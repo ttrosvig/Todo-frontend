75416c65dd68589d61759c3b72660480867982ad
Tristan <ttrosvig804@gmail.com>
2021-01-31 19:39:07 -0600

    addedd functionality to add a folder

---
+++ src/components/AddFolder.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { REACT_APP_BASE_URL } from '../variables';
+
+interface IAddFolderProps {
+	addFunc: () => void;
+}
+
+const AddFolder = ({ addFunc }: IAddFolderProps) => {
+	const [ formData, setFormData ] = useState('');
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		setFormData(event.target.value);
+	};
+
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		await axios.post(`${REACT_APP_BASE_URL}/folders`, { name: formData });
+
+		setFormData('');
+	};
+
+	return (
+		<form className="flex flex-row justify-center" onSubmit={handleSubmit}>
+			<input type="text" className="p-2 w-4/6" onChange={handleChange} value={formData} />
+
+			<button className="p-2 bg-blue-900 text-white rounded w-1/6">
+				<i className="fas fa-check" />
+			</button>
+			<button onClick={addFunc} className="py-2 px-3 bg-blue-900 rounded text-white w-1/6">
+				<i className="fas fa-times" />
+			</button>
+		</form>
+	);
+};
+
+export default AddFolder;
--- src/components/FolderList.tsx
+++ src/components/FolderList.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { REACT_APP_BASE_URL } from '../variables';
 import Folder from './Folder';
+import AddFolder from './AddFolder';
 
 interface IFolder {
 	name: string;
@@ -9,6 +10,11 @@ interface IFolder {
 
 const FolderList = () => {
 	const [ folders, setFolders ] = useState<any>([]);
+	const [ isAdding, setIsAdding ] = useState(false);
+
+	const toggleAdding = () => {
+		setIsAdding((isAdding) => !isAdding);
+	};
 
 	useEffect(
 		() => {
@@ -24,6 +30,16 @@ const FolderList = () => {
 
 	return (
 		<div>
+			{isAdding ? (
+				<AddFolder addFunc={toggleAdding} />
+			) : (
+				<div className="flex flex-row justify-end">
+					<button className="py-2 px-3 bg-blue-900 rounded-full text-white" onClick={toggleAdding}>
+						<i className="fas fa-plus" />
+					</button>
+				</div>
+			)}
+
 			{folders.map((folder: IFolder, idx: number) => {
 				return <Folder key={idx} name={folder.name} />;
 			})}
