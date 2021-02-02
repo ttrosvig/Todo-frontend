6c7f6fd640a594c827a007ab08f7707391cb4c3d
Tristan <ttrosvig804@gmail.com>
2021-02-02 10:21:38 -0600

    fixed title typo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 	return (
 		<div className="bg-blue-200 h-screen">
 			<div className="flex flex-col items-center">
-				<h1 className="font-bold text-blue-900 text-4xl my-4">Todo App/Bug Tracker</h1>
+				<h1 className="font-bold text-blue-900 text-4xl my-4">To-do App/Bug Tracker</h1>
 				<Routes />
 			</div>
 		</div>
